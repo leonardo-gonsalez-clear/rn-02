1d8c90bf9c8941b9b7cb8e59912c8a7794953790
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-04 11:37:18 -0300

    ✅ 05. 78: add game logic 2

--- 05-minesweeper/components/functions.tsx
+++ 05-minesweeper/components/functions.tsx
@@ -37,4 +37,57 @@ const createMinedBoard = (rows: number, columns: number, minesAmount: number): I
   return board
 }
 
-export { createMinedBoard as default }
+const cloneBoard = (board: ICell[][]): ICell[][] => {
+  return board.map(rows => {
+    return rows.map(field => {
+      return { ...field }
+    })
+  })
+}
+
+const getNeighbors = (board: ICell[][], row: number, column: number): ICell[] => {
+  const neighbors: ICell[] = []
+  const rows = [row - 1, row, row + 1]
+  const columns = [column - 1, column, column + 1]
+
+  rows.forEach(r => {
+    columns.forEach(c => {
+      const different = r !== row || c !== column
+      const validRow = r >= 0 && r < board.length
+      const validColumn = c >= 0 && c < board[0].length
+      if (different && validRow && validColumn) {
+        neighbors.push(board[r][c])
+      }
+    })
+  })
+
+  return neighbors
+}
+
+const safeNeighborhood = (board: ICell[][], row: number, column: number): boolean => {
+  return getNeighbors(board, row, column).reduce((result, neighbor) => result && !neighbor.mined, true)
+}
+
+const openField = (board: ICell[][], row: number, column: number) => {
+  const field = board[row][column]
+  if (!field.opened) {
+    field.opened = true
+    if (field.mined) {
+      field.exploded = true
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column).forEach(n => openField(board, n.row, n.column))
+    } else {
+      const neighbors = getNeighbors(board, row, column)
+      field.nearMines = neighbors.filter(n => n.mined).length
+    }
+  }
+}
+
+const fields = (board: ICell[][]) => board.reduce((rows, row) => rows.concat(row), [])
+const hadExplosion = (board: ICell[][]) => fields(board).filter(field => field.exploded).length > 0
+
+const pendding = (field: ICell) => (field.mined && !field.flagged) || (!field.mined && !field.opened)
+const wonGame = (board: ICell[][]) => fields(board).filter(pendding).length === 0
+const showMines = (board: ICell[][]) => fields(board).filter(field => field.mined).forEach(field => field.opened = true)
+
+export { createMinedBoard as default, cloneBoard, openField, hadExplosion, wonGame, showMines }
