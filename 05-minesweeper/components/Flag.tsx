083335241a71a2ae185e3ab6c156a2bdbe2fb2d4
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-04 10:16:02 -0300

    ✅ 05. 72: add bigger flag

--- 05-minesweeper/components/Flag.tsx
+++ 05-minesweeper/components/Flag.tsx
@@ -1,13 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Flag = () => {
+
+interface Props {
+  bigger?: boolean
+}
+
+const Flag = ({ bigger }: Props) => {
   return (
     <View style={styles.container}>
-      <View style={styles.flagPole}></View>
-      <View style={styles.flag}></View>
-      <View style={styles.base1}></View>
-      <View style={styles.base2}></View>
+      <View style={[styles.flagPole, bigger && styles.flagPoleBigger]}></View>
+      <View style={[styles.flag, bigger && styles.flagBigger]}></View>
+      <View style={[styles.base1, bigger && styles.base1Bigger]}></View>
+      <View style={[styles.base2, bigger && styles.base2Bigger]}></View>
     </View>
   )
 }
@@ -49,4 +54,26 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginTop: 12
   },
+  flagPoleBigger: {
+    height: 28,
+    width: 4,
+    marginLeft: 16,
+  },
+  flagBigger: {
+    height: 10,
+    width: 14,
+    marginLeft: 3,
+  },
+  base1Bigger: {
+    height: 4,
+    width: 12,
+    marginTop: 20,
+    marginLeft: 12,
+  },
+  base2Bigger: {
+    height: 4,
+    width: 20,
+    marginLeft: 8,
+    marginTop: 24,
+  }
 })
