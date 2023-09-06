43532cfdf66eacadd50b004dd3b19d0b59c52e13
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-06 10:04:49 -0300

    ✅ 06. 94-95: open drawer programmatically

--- 06-navigation/app/TelaC.tsx
+++ 06-navigation/app/TelaC.tsx
@@ -2,15 +2,19 @@ import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
 import { useLocalSearchParams, useRouter } from 'expo-router'
+import { DrawerActions } from '@react-navigation/native'
+import { useNavigation } from 'expo-router/src/useNavigation'
 
 const TelaC = () => {
   const router = useRouter()
+  const nav = useNavigation()
   const { num } = useLocalSearchParams()
   console.log(num)
 
   return (
     <View style={{ flex: 1 }}>
       <TextoCentral color='#0f0'>TELA C = {num}</TextoCentral>
+      <Button title='Abrir drawer' onPress={() => nav.dispatch(DrawerActions.openDrawer())} />
       <Button title='Avançar' onPress={() => {
         router.push({ pathname: "/TelaC", params: { num: String(Math.random() * 100) } })
       }} />
