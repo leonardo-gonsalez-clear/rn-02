108709e666b5282c64e0d9e52bc37ebc87babe74
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-06 11:55:58 -0300

    ✅ 07. 105: delete users

--- 07-crud/app/(stacks)/UserList.tsx
+++ 07-crud/app/(stacks)/UserList.tsx
@@ -1,24 +1,32 @@
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, Button } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import { FlatList } from 'react-native-gesture-handler';
 import users from '../../data/users';
 import { useRouter } from 'expo-router';
+import React from 'react';
 
 export default function TabOneScreen() {
   const router = useRouter()
+  const [data, setData] = React.useState(users)
+
   return (
     <View style={styles.container}>
-      <FlatList contentContainerStyle={{ gap: 16 }} keyExtractor={({ id }) => String(id)} data={users} renderItem={({ item }) => (
+      <FlatList contentContainerStyle={{ gap: 16 }} keyExtractor={({ id }) => String(id)} data={data} renderItem={({ item }) => (
         <TouchableOpacity
           style={{ flexDirection: "row", gap: 8 }}
           onPress={() => router.push({ pathname: "/(stacks)/UserForm", params: { user: item.name } })}
         >
           <Image source={{ uri: item.avatarUrl, width: 75, height: 75 }} style={{ borderRadius: 8 }} />
-          <View style={{ gap: 4 }}>
+          <View style={{ gap: 4, flex: 1 }}>
             <Text>{item.name}</Text>
             <Text style={{ color: "#777" }}>{item.email}</Text>
+          </View>
+
+          <View style={{ flexDirection: "row", gap: 8, alignItems: "flex-start" }}>
+            <Button title='E' color={"#00f"} />
+            <Button title='D' color={"#f00"} onPress={() => setData(prev => prev.filter(i => i !== item))} />
           </View>
         </TouchableOpacity>
       )} />
