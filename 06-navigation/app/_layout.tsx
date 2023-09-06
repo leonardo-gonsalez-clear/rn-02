793a17c2b4519474c0fee505fac5a613120ce997
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-06 10:19:35 -0300

    ✅ 06. 96-98: adding custom icons

--- 06-navigation/app/_layout.tsx
+++ 06-navigation/app/_layout.tsx
@@ -5,6 +5,7 @@ import { SplashScreen, Stack, Tabs } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { Drawer } from "expo-router/drawer"
+import { Octicons } from '@expo/vector-icons';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -42,17 +43,37 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+
+const icons = (color: string, size: number) => {
+
+  return {
+    TelaA: <Octicons name='code' color={color} size={size} />,
+    TelaB: <Octicons name='mail' color={color} size={size} />,
+    TelaC: <Octicons name='sync' color={color} size={size} />,
+  }
+}
+
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Drawer /* screenOptions={{ contentStyle: { flex: 1 } }} */ initialRouteName='TelaA'>
-        <Drawer.Screen name='index' redirect />
-        <Drawer.Screen name="TelaA" />
-        <Drawer.Screen name="TelaB" />
-        <Drawer.Screen name="TelaC" />
-      </Drawer>
+      {/* <Drawer initialRouteName = 'TelaA' > * /}
+      {/*   <Drawer.Screen name='index' redirect /> */}
+      {/*   <Drawer.Screen name="TelaA" /> */}
+      {/*   <Drawer.Screen name="TelaB" /> */}
+      {/*   <Drawer.Screen name="TelaC" /> */}
+      {/* </Drawer> */}
+      <Tabs initialRouteName='TelaA' screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          return icons(color, size)[route.name as keyof typeof icons]
+        }
+      })}>
+        <Tabs.Screen name='index' redirect />
+        <Tabs.Screen name="TelaA" />
+        <Tabs.Screen name="TelaB" />
+        <Tabs.Screen name="TelaC" />
+      </Tabs>
     </ThemeProvider>
   );
 }
