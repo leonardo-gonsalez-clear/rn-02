c1d8f9aef784307d4922557a527b41daca06cc4a
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-15 10:08:32 -0300

    ✅ 09. 158-161: update typescript types

--- 09-tasks-backend/src/@types/global.d.ts
+++ 09-tasks-backend/src/@types/global.d.ts
@@ -1,10 +1,27 @@
 import { Knex } from "knex";
 import Express from "express";
 
+interface IUser {
+  id: number
+  name: string
+  email: string
+}
+
+interface ITask {
+  id: number
+  description: string
+  doneAt: Date | null
+  estimateAt: Date
+  userId: number
+}
+
 declare global {
   namespace Express {
     interface Locals {
       db: Knex
     }
+
+    interface User extends IUser { }
+
   }
 }
