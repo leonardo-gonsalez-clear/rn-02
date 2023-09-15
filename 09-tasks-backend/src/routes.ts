0b97d49d65fbe8708078c021927d991ae45cb0a8
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-15 10:10:03 -0300

    ✅ 09. 158-161: register routes & add middlewares

--- 09-tasks-backend/src/routes.ts
+++ 09-tasks-backend/src/routes.ts
@@ -1,13 +1,19 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import UserController from "./controllers/UserController";
 import AuthController from "./controllers/AuthController";
 import "express-async-errors"
+import TasksController from "./controllers/TasksController";
 import passport from "./config/passport";
 
 const routes = Router()
 
 routes.post("/user", UserController.store)
 
-routes.post("/signin", AuthController.signIn)
+routes.post("/signin", /* (req, res, next) => passport(req, res, next).authenticate(), */ AuthController.signIn)
+
+routes.get("/tasks", passport.authenticate(), TasksController.index)
+routes.post("/tasks", passport.authenticate(), TasksController.store)
+routes.put("/tasks/:id", passport.authenticate(), TasksController.updateTask)
+routes.patch("/tasks/:id", passport.authenticate(), TasksController.doneTask)
 
 export default routes
