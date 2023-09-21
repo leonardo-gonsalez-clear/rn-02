b1fd9ae076d1e77b8196a9e22612088ed81b09fe
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-21 09:49:38 -0300

    ✅ 11. 203: add user store

---
+++ 11-instagram-clone/stores/useUserStore.tsx
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+
+interface Props {
+  user: null | IUser
+  setUser: (user: IUser) => void
+  logout: () => void
+}
+
+const useUserStore = create<Props>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  logout: () => set({ user: null }),
+}))
+
