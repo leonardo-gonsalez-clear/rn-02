963a4e42e8872830d1d6df688eedd241aae5f2ef
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-26 09:25:45 -0300

    ✅ 17. 251: own for each

--- 17-array/forEach.js
+++ 17-array/forEach.js
@@ -8,4 +8,14 @@ aprovados.forEach(nome => console.log(nome))
 
 const exibirAprovados = aprovado => console.log(aprovado)
 
-aprovaods.forEach(exibirAprovados)
+aprovados.forEach(exibirAprovados)
+
+const forEach = (array, callback) => {
+  const width = array.length
+
+  for (let i = 0; i < width; i++) {
+    callback(array[i], i, array)
+  }
+}
+
+forEach(aprovados, (nome, indice) => console.log(`${indice + 1}) ${nome}`))
