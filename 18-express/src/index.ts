6ae04d21a6f3aab8c2926b5053e05f73ecea4b8a
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-26 11:28:39 -0300

    ✅ 18. 271: reading data from request

--- 18-express/src/index.ts
+++ 18-express/src/index.ts
@@ -2,8 +2,19 @@ import Express from "express"
 
 const app = Express()
 
+app.use(Express.json())
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World" })
+  res.send(`
+<h1>Hello World</h1>
+<p>My name is <strong>John</strong></p>
+`)
+})
+
+app.post("/", (req, res) => {
+  const { name } = req.body
+
+  res.json(name)
 })
 
 app.listen(3000, () => console.log("server is running on port 3000"))
