24dce104a84793799174f07995daaa7665d1cc80
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-09-26 10:08:29 -0300

    ✅ 17. 256: filter

---
+++ 17-array/filter.js
@@ -0,0 +1,22 @@
+const produtos = [
+  { nome: 'Notebook', preco: 2499, fragil: true },
+  { nome: 'iPad Pro', preco: 4199, fragil: true },
+  { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+  { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+Array.prototype.newFilter = function(callback) {
+  const newArray = []
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i])
+    }
+  }
+
+  return newArray
+}
+
+const caro = produtos.newFilter(p => p.preco >= 2000)
+
+console.log(caro)
